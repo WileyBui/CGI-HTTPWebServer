61a355dd19828d0c0847770fbee0de32ba9c2348
Wiley Bui <17buih@gmail.com>
2020-03-17 18:27:42 -0500

    Connecting config file to server.c

--- src/server.c
+++ src/server.c
@@ -5,50 +5,73 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT 8080
-#define QLEN 10 // maximum server request queue length
-
 int main(int argc, char const *argv[])
 {
+  // will be imported from httpd.conf file
+  char root_directory[50];
+  char index_filename[50];
+  int simultaneous_connections, port;
+
+  FILE *ptr = fopen("../conf/httpd.conf", "r");
+  if (ptr == NULL) {
+    printf("Error opening up configuration file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  fscanf(ptr, "%i", &simultaneous_connections);
+  fscanf(ptr, "%s", root_directory);
+  fscanf(ptr, "%s", index_filename);
+  fscanf(ptr, "%i", &port);
+  fclose(ptr); // closing the file
+
+  printf("Server settings from /conf/httpd.conf:\n");
+  printf("  Number of simultaneous connections: %i\n",
+         simultaneous_connections);
+  printf("  Root directory (to start looking for HTML files): %s\n",
+         root_directory);
+  printf("  Index filename (if none given): %s\n", index_filename);
+  printf("  Port to run on server: %i\n", port);
+
   char buffer[1024] = {0};
-  struct sockaddr_in address;             // an Internet endpoint address
-  int s, new_socket, client_header_value; // socket descriptor, s
+  int s, new_sock, client_header_value; // socket descriptor, s
+  struct sockaddr_in address;           // an Internet endpoint address
   socklen_t sock_size = sizeof(struct sockaddr_in);
 
   address.sin_family      = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port        = htons(PORT); // byte order
+  address.sin_port        = htons(port); // byte order
 
   // Allocate a socket
   s = socket(AF_INET, SOCK_STREAM, 0); // TCP uses SOCK_STREAM
   if (s < 0) {
-    printf("Socket failed - can't be created.");
+    printf("Socket failed - can't be created.\n");
     exit(EXIT_FAILURE);
   }
 
   // Bind the socket
   if (bind(s, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    printf("Socket binding failed.");
+    printf("Socket binding failed.\n");
     exit(EXIT_FAILURE);
   }
 
   // Put server to passive mode
-  if (listen(s, QLEN) < 0) {
-    printf("Listening failed.");
+  if (listen(s, simultaneous_connections) < 0) {
+    printf("Listening failed.\n");
     exit(EXIT_FAILURE);
   }
 
+  // Accepting new connections
   while (1) {
-    new_socket = accept(s, (struct sockaddr *)&address, &sock_size);
+    new_sock = accept(s, (struct sockaddr *)&address, &sock_size);
 
-    if (new_socket < 0) {
-      printf("New socket failed.");
+    if (new_sock < 0) {
+      printf("New socket failed.\n");
       exit(EXIT_FAILURE);
     }
     else {
-      client_header_value = read(new_socket, buffer, 1024);
+      client_header_value = read(new_sock, buffer, 1024);
       printf("Received from the client: %s\n", buffer);
-      close(new_socket);
+      close(new_sock);
     }
   }
   close(s);
